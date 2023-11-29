0e33830fbe78df8a22f814cb8b4a0ef0b4a903fa
lw86ruwo <Leon@wenderoth.de>
2023-11-29 18:06:12 +0100

    adj naming and adding expiration to token

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+import datetime
 from enum import Enum
 from dataclasses import dataclass
+from typing import Any
 
 import jwt
 
@@ -13,18 +15,20 @@ class Authorisation(Enum):
 jwtkey = "secret"
 
 
-def encode(obj):
+def encode(obj: dict[str, Any]):
 	return jwt.encode(obj, jwtkey, algorithm="HS256")
 
 
 def decode(string: str):
-	token: User = jwt.decode(string, jwtkey, algorithm="HS256")
-	return User(token.user, token.id)
+	token: Token = jwt.decode(string, jwtkey, leeway=datetime.timedelta(minutes=1), algorithm="HS256", verify=True)
+	return Token(token.user, token.id, token.exp)
 
 
 @dataclass
-class User:
+class Token:
 	user: str
 	id: int
+	exp = datetime.datetime.now() + datetime.timedelta(hours=1)
 
-
+	def dict(self):
+		return {"user": self.user, "id": self.id}
