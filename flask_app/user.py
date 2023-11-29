9ae18fdcd43f3b37f048065860502bbda0ec1546
lw86ruwo <Leon@wenderoth.de>
2023-11-29 18:08:04 +0100

    add register and login

--- flask_app/user.py
+++ flask_app/user.py
@@ -1,13 +1,14 @@
 # main_routes.py
 from flask import Blueprint, request, jsonify
 
-from config import User, encode
+from config import Token, encode
+from postgres.queries import checkPassword
 from postgres.transactions import addUser
 
 user_management = Blueprint('user_management', __name__)
 
 
-@user_management.route('/registerrrrrrrrrrrrrrrrrr', methods=['POST'])
+@user_management.route('/register', methods=['GET', 'POST'])
 def register():
 	data = request.get_json()
 	username = data.get('username')
@@ -15,10 +16,29 @@ def register():
 
 	_id = addUser(username, password)
 
-	if id:
-		user = User(username, _id)
-		token = encode(user)
+	if _id:
+		user = Token(username, _id)
+		token = encode(user.dict())
+
 		return jsonify({'message': 'User registered successfully',
 						'token': token})
 
-	return jsonify({'message': 'User registered successfully'})
+	return jsonify({'message': 'User register failed'})
+
+
+@user_management.route('/login', methods=['GET', 'POST'])
+def login():
+	data = request.get_json()
+	username = data.get('username')
+	password = data.get('password')
+
+	_correct = checkPassword(username, password)
+
+	if _correct:
+		user = Token(username, _correct)
+		token = encode(user.dict())
+
+		return jsonify({'message': 'Log in successfully',
+						'token': token})
+	else:
+		return jsonify({'message': 'Wrong Password'})
