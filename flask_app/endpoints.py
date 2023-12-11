f080d7c6e157c27c32ce793443d3795d3cf23ad5
lw86ruwo <Leon@wenderoth.de>
2023-12-11 20:18:33 +0100

    add file upload

--- flask_app/endpoints.py
+++ flask_app/endpoints.py
@@ -1,9 +1,33 @@
-# main_routes.py
-from flask import Blueprint
+from flask import Blueprint, request,jsonify
+from werkzeug.datastructures import FileStorage
+from werkzeug.utils import secure_filename
+from config import decode
 
-main_routes = Blueprint('main_routes', __name__)
+from postgres.transactions import addDocument
+
+main_routes = Blueprint('main_routes', __name__, url_prefix='/data')
 
 
-@main_routes.route('/api')
-def hello_world():
-	return "Hello a"
+@main_routes.route('/upload', methods=['POST'])
+def upload_files():
+	try:
+		files = request.files
+		form = request.form
+
+		authorization = form.get("authorization")
+		organisation_id = form.get("organisationid")
+
+		token = decode(authorization)
+
+		dokument_ids: list[int] = []
+
+		for _filename, storage in files.items():
+			filename = secure_filename(_filename)
+			t = storage.read()
+			dokument_id = addDocument(filename, t, organisation_id, token.id)
+			dokument_ids.append(dokument_id)
+
+		return jsonify(dokument_ids)
+
+	except Exception as e:
+		raise Exception("upload files failed because: \n", e)
