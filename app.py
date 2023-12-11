8b73a192663a608666e299760ab332b408e6c6e9
lw86ruwo <Leon@wenderoth.de>
2023-12-11 15:24:37 +0100

    create vev_routes and reformat

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 # app.py
 from flask import Flask
 from flask_cors import CORS
+
+from flask_app.dev import dev_routes
 from flask_app.endpoints import main_routes
 from flask_app.user import user_management
-from postgres.transactions import createTables, dropTables
-
-
 
 app = Flask(__name__)
 CORS(app)
@@ -13,9 +12,8 @@ CORS(app)
 # Register the blueprints
 app.register_blueprint(main_routes)
 app.register_blueprint(user_management)
+app.register_blueprint(dev_routes)
 
-# TODO create table probably not here?
-createTables()
 
 @app.route('/')
 def hello_world():
@@ -23,7 +21,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
-        
-      
-
+	app.run(host='0.0.0.0', port=8000, debug=True)
---
+++ flask_app/dev.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, request
+from werkzeug.datastructures import FileStorage
+from werkzeug.utils import secure_filename
+
+from postgres.transactions import createUserTable
+from config import decode
+
+dev_routes = Blueprint('dev_routes', __name__, url_prefix='/dev')
+
+
+@dev_routes.route('/createTables', methods=['POST'])
+def createTables():
+	try:
+		createUserTable()
+	finally:
+		# Remove the temporary files
+		for file in files:
+			filename = secure_filename(file.filename)
+			if os.path.exists(filename):
+				os.remove(filename)
+
+	return 'Files uploaded successfully'
