9841e643721b53180b32030c3c6d63b36782556c
lw86ruwo <Leon@wenderoth.de>
2024-01-24 15:02:43 +0100

    add tasks

--- wannadb_web/worker/tasks.py
+++ wannadb_web/worker/tasks.py
@@ -95,29 +95,22 @@ class CreateDocumentBase(BaseTask):
 
 	def run(self, user_id: int, document_ids: list[int], attributes_strings: list[str], statistics_dump: bytes,
 			base_name: str, organisation_id: int):
-		self._signals = Signals(str(self.request.id))
-		self._redis_client = RedisCache(str(self.request.id))
 		self.load()
 		attributes: list[Attribute] = []
 		statistics: Statistics = pickle.loads(statistics_dump)
-		print("attributes_strings:", attributes_strings)
-  # convert attributes_strings to an array
-		attributes_strings = list[str](attributes_strings)
 		for attribute_string in attributes_strings:
-			print("attribute_string:", attribute_string)
 			if attribute_string == "":
 				logger.error("Attribute names cannot be empty!")
 				raise Exception("Attribute names cannot be empty!")
-			for attribute in attributes:
-				if attribute_string == attribute.name:
-					logger.error("Attribute names must be unique!")
-					raise Exception("Attribute names must be unique: " + attribute_string)
+			if attribute_string in [attribute.name for attribute in attributes]:
+				logger.error("Attribute names must be unique!")
+				raise Exception("Attribute names must be unique!")
 			attributes.append(Attribute(attribute_string))
 
 		"""
 		init api
 		"""
-		api = WannaDB_WebAPI(user_id, EmptyInteractionCallback(), base_name, organisation_id)
+		api = WannaDB_WebAPI(user_id, base_name, organisation_id)
 
 		"""
 		Creating document base
@@ -146,240 +139,100 @@ class CreateDocumentBase(BaseTask):
 		self.update(State.SUCCESS)
 		return self
 
-#
-#
-# @app.task(bind=True)
-# def add_attributes(self, user_id: int, attributes_dump: Optional[bytes], base_name: str, organisation_id: int):
-# 	"""
-# 	define values
-# 	"""
-#
-# 	def task_callback_fn(state: str, meta: TaskObject):
-# 		if isinstance(state, str) and state is not None and len(state) > 0:
-# 			meta_dump = meta.to_dump()
-# 			self.update_state(state=state, meta=meta_dump)
-# 		else:
-# 			raise Exception("task_callback_fn error Invalid state")
-#
-# 	task_callback = TaskCallback(task_callback_fn)
-#
-# 	task_object = TaskObject(task_callback)
-#
-# 	"""
-# 	init api
-# 	"""
-#
-# 	api = WannaDB_WebAPI(user_id, task_object, base_name, organisation_id)
-# 	task_object.check()
-#
-# 	api.load_document_base_from_bson()
-# 	task_object.check()
-#
-# 	if attributes_dump is not None:
-# 		attributes: list[Attribute] = pickle.loads(attributes_dump)
-# 		api.add_attributes(attributes)
-# 		if task_object.signals.error.msg:
-# 			task_object.update(State.FAILURE)
-# 			raise task_object.signals.error.msg
-#
-# 	api.save_document_base_to_bson()
-# 	task_object.check()
-# 	task_object.update(state=State.SUCCESS)
-#
-#
-# @app.task(bind=True)
-# def remove_attributes(self, user_id: int, attributes_dump: Optional[bytes], base_name: str, organisation_id: int):
-# 	"""
-# 	define values
-# 	"""
-#
-# 	def task_callback_fn(state: str, meta: TaskObject):
-# 		if isinstance(state, str) and state is not None and len(state) > 0:
-# 			meta_dump = meta.to_dump()
-# 			self.update_state(state=state, meta=meta_dump)
-# 		else:
-# 			raise Exception("task_callback_fn error Invalid state")
-#
-# 	task_callback = TaskCallback(task_callback_fn)
-#
-# 	task_object = TaskObject(task_callback)
-#
-# 	"""
-# 	init api
-# 	"""
-#
-# 	api = WannaDB_WebAPI(user_id, task_object, base_name, organisation_id)
-# 	task_object.check()
-#
-# 	api.load_document_base_from_bson()
-# 	task_object.check()
-#
-# 	if attributes_dump is not None:
-# 		attributes: list[Attribute] = pickle.loads(attributes_dump)
-# 		api.remove_attributes(attributes)
-# 		if task_object.signals.error.msg:
-# 			task_object.update(State.FAILURE)
-# 			raise task_object.signals.error.msg
-#
-# 	api.save_document_base_to_bson()
-# 	task_object.check()
-# 	task_object.update(state=State.SUCCESS)
-#
-#
-# @app.task(bind=True)
-# def forget_matches_for_attribute(self, user_id: int, attribute_dump: Optional[bytes], base_name: str,
-# 								 organisation_id: int):
-# 	"""
-# 	define values
-# 	"""
-#
-# 	def task_callback_fn(state: str, meta: TaskObject):
-# 		if isinstance(state, str) and state is not None and len(state) > 0:
-# 			meta_dump = meta.to_dump()
-# 			self.update_state(state=state, meta=meta_dump)
-# 		else:
-# 			raise Exception("task_callback_fn error Invalid state")
-#
-# 	task_callback = TaskCallback(task_callback_fn)
-#
-# 	task_object = TaskObject(task_callback)
-#
-# 	"""
-# 	init api
-# 	"""
-#
-# 	api = WannaDB_WebAPI(user_id, task_object, base_name, organisation_id)
-# 	task_object.check()
-#
-# 	api.load_document_base_from_bson()
-# 	task_object.check()
-#
-# 	if attribute_dump is not None:
-# 		attribute: Attribute = pickle.loads(attribute_dump)
-# 		api.forget_matches_for_attribute(attribute)
-# 		if task_object.signals.error.msg:
-# 			task_object.update(State.FAILURE)
-# 			raise task_object.signals.error.msg
-#
-# 	api.save_document_base_to_bson()
-# 	task_object.check()
-# 	task_object.update(state=State.SUCCESS)
-#
-#
-# @app.task(bind=True)
-# def forget_matches(self, user_id: int, base_name: str, organisation_id: int):
-# 	"""
-# 	define values
-# 	"""
-#
-# 	def task_callback_fn(state: str, meta: TaskObject):
-# 		if isinstance(state, str) and state is not None and len(state) > 0:
-# 			meta_dump = meta.to_dump()
-# 			self.update_state(state=state, meta=meta_dump)
-# 		else:
-# 			raise Exception("task_callback_fn error Invalid state")
-#
-# 	task_callback = TaskCallback(task_callback_fn)
-#
-# 	task_object = TaskObject(task_callback)
-#
-# 	"""
-# 	init api
-# 	"""
-#
-# 	api = WannaDB_WebAPI(user_id, task_object, base_name, organisation_id)
-# 	task_object.check()
-#
-# 	api.load_document_base_from_bson()
-# 	task_object.check()
-#
-# 	api.forget_matches()
-# 	task_object.check()
-#
-# 	api.save_document_base_to_bson()
-# 	task_object.check()
-# 	task_object.update(state=State.SUCCESS)
-#
-#
-# @app.task(bind=True)
-# def interactive_table_population(self, user_id: int, attributes_dump: Optional[bytes], base_name: str,
-# 								 organisation_id: int):
-# 	"""
-# 	define values
-# 	"""
-#
-# 	def task_callback_fn(state: str, meta: TaskObject):
-# 		if isinstance(state, str) and state is not None and len(state) > 0:
-# 			meta_dump = meta.update().to_dump()
-# 			self.update_state(state=state, meta=meta_dump)
-# 		else:
-# 			raise Exception("task_callback_fn error Invalid state")
-#
-# 	task_callback = TaskCallback(task_callback_fn)
-#
-# 	def interaction_callback_fn(pipeline_element_identifier, feedback_request):
-# 		feedback_request["identifier"] = pipeline_element_identifier
-# 		self.feedback_request_to_ui.emit(feedback_request)
-#
-# 		self.feedback_mutex.lock()
-# 		try:
-# 			self.feedback_cond.wait(self.feedback_mutex)
-# 		finally:
-# 			self.feedback_mutex.unlock()
-#
-# 		return self.feedback
-#
-# 	interaction_callback = InteractionCallback(interaction_callback_fn)
-#
-# 	task_object = TaskObject(task_callback)
-#
-# 	"""
-# 	init api
-# 	"""
-#
-# 	api = WannaDB_WebAPI(user_id, task_object, base_name, organisation_id)
-# 	task_object.check()
-#
-# 	api.load_document_base_from_bson()
-# 	task_object.check()
-#
-# 	api.forget_matches()
-# 	task_object.check()
-#
-# 	api.save_document_base_to_bson()
-# 	task_object.check()
-# 	task_object.update(state=State.SUCCESS)
-#
-#
-# @app.task(bind=True)
-# def long_task(self):
-# 	try:
-# 		"""Background task that runs a long function with progress reports."""
-# 		verb = ['Starting up', 'Booting', 'Repairing', 'Loading', 'Checking']
-# 		adjective = ['master', 'radiant', 'silent', 'harmonic', 'fast']
-# 		noun = ['solar array', 'particle reshaper', 'cosmic ray', 'orbiter', 'bit']
-# 		data = ''
-# 		total = random.randint(10, 50)
-#
-# 		def task_callback_fn(state: str, meta: TaskObject):
-# 			if not isinstance(state, str):
-# 				raise Exception("task_callback_fn error Invalid state")
-# 			meta_dump = meta.to_dump()
-# 			self.update_state(state=state, meta=meta_dump)
-#
-# 		task_callback = TaskCallback(task_callback_fn)
-#
-# 		task_object = TaskObject(task_callback)
-#
-# 		for i in range(total):
-# 			if not data or random.random() < 0.25:
-# 				data = '{0} {1} {2}...'.format(random.choice(verb),
-# 											   random.choice(adjective),
-# 											   random.choice(noun))
-# 			time.sleep(1)
-# 			task_object.update(state=State.PENDING)
-# 		task_object.update(state=State.SUCCESS)
-# 		return data
-# 	except Exception as e:
-# 		self.update_state(state=State.FAILURE.value, meta={'exception': str(e)})
-# 		raise
+
+class DocumentBaseAddAttributes(BaseTask):
+	name = "DocumentBaseAddAttributes"
+
+	def run(self, user_id: int, attributes_strings: list[str], base_name: str, organisation_id: int):
+		self.load()
+		attributes: list[Attribute] = []
+
+		for attribute_string in attributes_strings:
+			if attribute_string == "":
+				logger.error("Attribute names cannot be empty!")
+				raise Exception("Attribute names cannot be empty!")
+			if attribute_string in [attribute.name for attribute in attributes]:
+				logger.error("Attribute names must be unique!")
+				raise Exception("Attribute names must be unique!")
+			attributes.append(Attribute(attribute_string))
+
+		api = WannaDB_WebAPI(user_id, base_name, organisation_id)
+		api.load_document_base_from_bson()
+		api.add_attributes(attributes)
+		api.update_document_base_to_bson()
+
+
+class DocumentBaseRemoveAttributes(BaseTask):
+	name = "DocumentBaseRemoveAttributes"
+
+	def run(self, user_id: int, attributes_strings: list[str], base_name: str, organisation_id: int):
+		self.load()
+		attributes: list[Attribute] = []
+
+		for attribute_string in attributes_strings:
+			if attribute_string == "":
+				logger.error("Attribute names cannot be empty!")
+				raise Exception("Attribute names cannot be empty!")
+			if attribute_string in [attribute.name for attribute in attributes]:
+				logger.error("Attribute names must be unique!")
+				raise Exception("Attribute names must be unique!")
+			attributes.append(Attribute(attribute_string))
+
+		api = WannaDB_WebAPI(user_id, base_name, organisation_id)
+		api.load_document_base_from_bson()
+		api.remove_attributes(attributes)
+		if api.signals.error.msg is None:
+			api.update_document_base_to_bson()
+
+
+class DocumentBaseForgetMatches(BaseTask):
+	name = "DocumentBaseForgetMatches"
+
+	def run(self, user_id: int, attributes_strings: list[str], base_name: str, organisation_id: int):
+		self.load()
+		attributes: list[Attribute] = []
+
+		for attribute_string in attributes_strings:
+			if attribute_string == "":
+				logger.error("Attribute names cannot be empty!")
+				raise Exception("Attribute names cannot be empty!")
+			if attribute_string in [attribute.name for attribute in attributes]:
+				logger.error("Attribute names must be unique!")
+				raise Exception("Attribute names must be unique!")
+			attributes.append(Attribute(attribute_string))
+
+		api = WannaDB_WebAPI(user_id, base_name, organisation_id)
+		api.load_document_base_from_bson()
+		api.forget_matches()
+		if api.signals.error.msg is None:
+			api.update_document_base_to_bson()
+
+
+class DocumentBaseForgetMatchesForAttribute(BaseTask):
+	name = "DocumentBaseForgetMatches"
+
+	def run(self, user_id: int, attribute_string: str, base_name: str, organisation_id: int):
+		self.load()
+
+		attribute = (Attribute(attribute_string))
+
+		api = WannaDB_WebAPI(user_id, base_name, organisation_id)
+		api.load_document_base_from_bson()
+		api.forget_matches_for_attribute(attribute)
+		if api.signals.error.msg is None:
+			api.update_document_base_to_bson()
+
+
+class DocumentBaseInteractiveTablePopulation(BaseTask):
+	name = "DocumentBaseInteractiveTablePopulation"
+
+	def run(self, user_id: int, base_name: str, organisation_id: int):
+		self._signals = Signals(str(self.request.id))
+		self._redis_client = RedisCache(str(self.request.id))
+		self.load()
+
+		api = WannaDB_WebAPI(user_id, base_name, organisation_id)
+		api.load_document_base_from_bson()
+		api.interactive_table_population()
+		if api.signals.error.msg is None:
+			api.update_document_base_to_bson()
