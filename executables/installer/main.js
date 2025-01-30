5c7b7ce5c96bf9ec4b7df39a518be5b9dc4e3f41
Jannis25 <jannislb@gmail.com>
2025-01-30 14:46:00 +0100

    Refactor installer HTML and JavaScript for improved layout and user messaging

--- executables/installer/main.js
+++ executables/installer/main.js
@@ -35,9 +35,7 @@ function createWindow() {
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+    app.quit();
 });
 
 app.on('activate', () => {
